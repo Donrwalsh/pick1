8a44c6806d5025ded9920ee434abc03d208d570a
Don R Walsh <donrwalsh@gmail.com>
2019-03-14 09:43:56 -0500

    Maven Spring Boot Starter
    Empty Spring Initializr project to kick things off. Chose Maven based on not much at all. Brought in IntelliJ and Spring Boot .gitignore files from quick google searches.

---
+++ server/src/main/java/bigbrass/pick1/Pick1Application.java
@@ -0,0 +1,13 @@
+package bigbrass.pick1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pick1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pick1Application.class, args);
+	}
+
+}
